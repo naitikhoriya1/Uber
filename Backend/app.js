9d71011f5db2d7aef08612a62994c5f9962fefb0
Naitik khoriya <naitikkhoriya144263@gmail.com>
2024-11-27 11:30:16 +0530

    "Updated app.js to include new routes and middleware, added user existence check in user.controller.js, and modified auth.middleware.js to include separate authentication for captains and blacklisted tokens."

--- Backend/app.js
+++ Backend/app.js
@@ -1,21 +1,29 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const cookiesParser = require("cookie-parser");
-const connectToDb = require("./Db/db.js");
-const userRoutes = require("./routes/user.routes.js");
+const cookieParser = require("cookie-parser");
+const connectToDb = require("./Db/db");
+const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
+// const mapsRoutes = require("./routes/maps.routes");
+// const rideRoutes = require("./routes/ride.routes");
+
 connectToDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookiesParser());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World");
 });
 
 app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
+// app.use("/maps", mapsRoutes);
+// app.use("/rides", rideRoutes);
 
 module.exports = app;
